623d2817cbef8640732c8ee9d975a1a13304846f
Shiv-SB <shivarjun@hotmail.co.uk>
2024-05-08 21:50:38 +0100

    Update Problem15.js

--- Problem15.js
+++ Problem15.js
@@ -3,8 +3,12 @@ import {helper} from "./HelperFunctions.js";
 // Lattice Paths
 
 const arraySize = 2; 
+const destination = Math.pow(arraySize, 2) - 1;
+const vertStep = 1;
+const horizStep = arraySize;
 
 function createSquareGrid(size) {
+    size++;
     let grid = [];
     let h = 0;
     for (let i = 0; i < size; i++) {
@@ -18,4 +22,19 @@ function createSquareGrid(size) {
 
 let squareGrid = createSquareGrid(arraySize);
 
+squareGrid.forEach((row, i)=> {
+//
+})
+
 console.log(squareGrid);
+console.log(destination);
+
+/*
+Grid mapping:
+Grid      Routes
+1           2
+2           6
+3           
+*/
+
+//https://www.interactive-maths.com/blog/restricted-taxicab-geometry
