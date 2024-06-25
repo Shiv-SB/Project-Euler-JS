7a3a9eae1d2d75b370ad4c69ad1ed1904ed7727b
Shiv-hcr <167079300+Shiv-hcr@users.noreply.github.com>
2024-06-25 16:59:21 +0100

    Create Problem31.ts

---
+++ src/Problem31.ts
@@ -0,0 +1,13 @@
+// Coin Sums
+
+const coins: number[] = [
+    1, 2, 5, 10, 20, 50, 100, 200
+];
+
+const goal: number = 50;
+
+// https://en.wikipedia.org/wiki/Change-making_problem
+
+function changeCombos(coins: number[], goal: number) {
+    
+}
